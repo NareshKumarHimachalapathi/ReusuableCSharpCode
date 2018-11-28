5194bdf5eb79e14059d16d2bedefa5af5a9a6174
NareshKumarHimachalapathi <hnareshkumar86@gmail.com>
2018-11-28 15:24:24 +0530

    Update RetryHelper.cs

--- RetryHelper.cs
+++ RetryHelper.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Infosys.AINA.EmotionDetection.VideoProcessor
+namespace VideoProcessor
 {
     public static class RetryHelper
     {
