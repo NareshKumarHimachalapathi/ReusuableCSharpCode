fe475daf47dc001f9bef3d41de161b33674c68cd
NareshKumarHimachalapathi <hnareshkumar86@gmail.com>
2018-11-28 15:07:34 +0530

    Create RetryHelper.cs

---
+++ RetryHelper.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infosys.AINA.EmotionDetection.VideoProcessor
+{
+    public static class RetryHelper
+    {
+          public static void RetryOnException(int times, TimeSpan delay, Action operation)
+        {
+            var attempts = 0;
+            do
+            {
+                try
+                {
+                    attempts++;
+                    operation();
+                    break; // Sucess! Lets exit the loop!
+                }
+                catch (Exception ex)
+                {
+                    if (attempts == times)
+                        throw;           
+
+                    Task.Delay(delay).Wait();
+                }
+            } while (true);
+        }
+    }
+}
